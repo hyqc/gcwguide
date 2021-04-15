b2102d1abc6240b3b6b6ca3c6bcb60589659c5da
weilpongpei <1074966185@qq.com>
2021-04-16 01:52:24 +0800

    添加前端

---
+++ gcweb/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <el-container class="container">
+    <el-header class="header">Header</el-header>
+    <el-main class="main">
+      <web-list />
+    </el-main>
+    <el-footer class="footer">Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+import WebList from "./components/WebList.vue";
+
+export default {
+  name: "App",
+  components: {
+    WebList,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+}
+.body {
+  padding-bottom: 60px;
+}
+.container {
+  width: 100%;
+  min-height: 100%;
+  position: relative;
+  background: white;
+  display: block;
+}
+.header,
+.footer {
+  height: 60px;
+  width: 100%;
+  display: block;
+  border-bottom: 1px solid rgb(180, 184, 176);
+}
+.main {
+  display: block;
+  overflow: auto;
+}
+.footer {
+  height: 60px;
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+  text-align: center;
+  border-top: 1px solid rgb(180, 184, 176);
+}
+.backtop {
+  height: 100vh;
+  overflow-x: hidden;
+}
+</style>
---
+++ gcweb/src/components/WebList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <el-card class="box-card" shadow="never">
+      <template #header>
+        <div class="card-header">
+          <span>默认</span>
+          <el-button class="button" type="text">编辑</el-button>
+        </div>
+      </template>
+      <div v-for="o in 4" :key="o" class="text item">
+        <el-row :gutter="48">
+          <el-col :span="6">
+            <el-card shadow="hover" class="web-item">
+              总是显示3
+            </el-card>
+          </el-col>
+          <el-col :span="6">
+            <el-card shadow="hover" class="web-item">
+              总是显示4
+            </el-card>
+          </el-col>
+          <el-col :span="6">
+            <el-card shadow="hover" class="web-item">
+              鼠标悬浮时显示5
+            </el-card>
+          </el-col>
+          <el-col :span="6">
+            <el-card shadow="hover" class="web-item">
+              从不显示6
+            </el-card>
+          </el-col>
+        </el-row>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.web-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+  text-align: center;
+}
+.el-row {
+  margin: 2rem;
+}
+</style>
